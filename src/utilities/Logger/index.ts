0acdd2b06cc78557a6ed93a076a931a04f0aa464
DanfordGidraph <danfordmaina@gmail.com>
2024-08-26 13:11:28 +0300

    * feat(Logger): add Logger utility module with winston configuration

---
+++ src/utilities/Logger/index.ts
@@ -0,0 +1,37 @@
+import winston from "winston";
+
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  debug: 4,
+};
+
+const colors = {
+  error: "red",
+  warn: "yellow",
+  info: "green",
+  http: "magenta",
+  debug: "white",
+};
+
+winston.addColors(colors);
+const format = winston.format.combine(
+  winston.format.colorize({ all: true }),
+  winston.format.timestamp(),
+  winston.format.align(),
+  winston.format.printf((info) => {
+    const {
+      timestamp, level, message, ...args
+    } = info;
+
+    const ts = timestamp.slice(0, 19).replace('T', ' ');
+    return `${ts} [${level}]: ${message} ${Object.keys(args).length ? JSON.stringify(args, null, 2) : ''}`;
+  }),
+);
+
+const transports = [new winston.transports.Console()];
+const Logger = winston.createLogger({ levels, format, transports, });
+
+export default Logger;
